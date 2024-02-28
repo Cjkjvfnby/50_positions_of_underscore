e656ee20b19a5a96213ed308ef6c0f67bbb0e065
Cjkjvfnby <Cjkjvfnby@users.noreply.github.com>
2024-02-28 00:51:46 +0000

    deploy: a6f83395e50fcbc7cab7283445a52a336df41fe2

--- example/dundr_predefined.py
+++
@@ -1,8 +0,0 @@
-"""
-Example for dundr methods and attributes
-"""
-
-# Special variable to access current context
-if __name__ == "__main__":
-    print(f"File {__file__}")
-    print(f"This is docs: {__doc__}")
--- example/middle.py
+++
@@ -1,6 +0,0 @@
-KRAKEN_IS_FREE = False
-
-
-def release_the_kraken():
-    global KRAKEN_IS_FREE
-    KRAKEN_IS_FREE = True
